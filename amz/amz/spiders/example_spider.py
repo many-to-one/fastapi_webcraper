d5dd5af3d2930312687d7e3539f227306908077d
many-to-one <xllxlex@gmail.com>
2025-04-18 13:50:42 +0200

    add templates

--- amz/amz/spiders/example_spider.py
+++ amz/amz/spiders/example_spider.py
@@ -2,17 +2,23 @@ import scrapy
 import pandas as pd
 
 class ExampleSpider(scrapy.Spider):
+
     name = "amazon"
     allowed_domains = ["amazon.pl"]
     # start_urls = ["https://www.amazon.com/s?k=laptops&page=22"] # Wyszukiwarka
     # start_urls = ["https://www.amazon.com/s?k=zabawki&page=8"] # i td
-    start_urls = ["https://www.amazon.com/s?k=tablets&rh=n%3A1232597011%2Cp_n_size_browse-bin%3A7817235011&crid=1Y6QHLGWI6DP1&nav_sdd=aps&rnid=1254615011&sprefix=tablets&ref=nb_sb_ss_w_sbl-tr-t1_k0_1_7_0"]
+    # start_urls = ["https://www.amazon.com/s?k=tablets&rh=n%3A1232597011%2Cp_n_size_browse-bin%3A7817235011&crid=1Y6QHLGWI6DP1&nav_sdd=aps&rnid=1254615011&sprefix=tablets&ref=nb_sb_ss_w_sbl-tr-t1_k0_1_7_0"]
+
+    def __init__(self, url=None, *args, **kwargs):
+       super().__init__(*args, **kwargs)
+       self.start_urls = [url] if url else ["https://default-url.com"]  # Use given URL or fallback
 
     def parse(self, response):
 
         if response.status == 403:
             self.log("Access forbidden - 403 error.")
             return
+        
         products = []
         for product in response.css('div.s-main-slot div.s-result-item'):
             products.append(
--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-from fastapi import FastAPI, BackgroundTasks 
+from fastapi import FastAPI, BackgroundTasks, Request, Form
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+from urllib.parse import unquote, quote
 import subprocess
 import pandas as pd
 import plotly.express as px
@@ -7,24 +11,54 @@ import plotly.express as px
 
 app = FastAPI()
 
-@app.get("/scrape/{spider_name}") 
-async def run_spider(spider_name: str, background_tasks: BackgroundTasks): 
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/")
+async def root(request: Request, response=HTMLResponse):
+    return templates.TemplateResponse(
+        request=request, name="home.html", context={"id": id}
+    )
+
+@app.post("/scrape") 
+async def run_spider(background_tasks: BackgroundTasks, url: str = Form(...)): 
+
+    # url="https://www.amazon.com/s?k=iphone+16+pro+case&crid=2HY21R9MW4WN7&sprefix=Iphone%2Caps%2C375&ref=nb_sb_ss_ts-doa-p_2_6"
+
+    # Decode URL from FastAPI path
+    decoded_url = unquote(url)
+
+
+    print(f"Decoded URL: {decoded_url}")
+    spider_name = "amazon"
 
     SCRAPY_PROJECT_PATH = "./amz"
     print('SCRAPY_PROJECT_PATH', SCRAPY_PROJECT_PATH)
 
-    command = ["scrapy", "crawl", spider_name]
+    # command = ["scrapy", "crawl", spider_name]
+
+    # Pass the URL as an argument to the spider
+    command = ["scrapy", "crawl", spider_name, "-a", f"url={decoded_url}"]
+
     background_tasks.add_task(subprocess.Popen, command, cwd=SCRAPY_PROJECT_PATH) 
     return {"message": f"Started spider {spider_name}"}
 
 
-@app.get('/')
-async def root():
+@app.get('/vis')
+async def visualization():
 
     df = pd.read_excel("templates/products.xlsx")
     df["price"] = pd.to_numeric(df["price"], errors="coerce")
 
-    fig = px.bar(df.nlargest(10, "price"), x="title", y="price", color="rating", title="Top 10 Most Expensive Products")
+    # fig = px.bar(df.nlargest(10, "price"), y="title", x="price", color="rating", title="Top 10 Most Expensive Products")
+    fig = px.bar(df.nlargest(10, "price"), 
+             y="title", x="price", color="rating",
+             title="Top 10 Most Expensive Products",
+             category_orders={"title": df.nlargest(10, "price")["title"].tolist()[::-1]})
+
+
 
     content = fig.to_html(full_html=True)
 
